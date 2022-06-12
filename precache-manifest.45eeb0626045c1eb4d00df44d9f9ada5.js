5e2753bf3060393452d66acc9f3ea3d618699d84
SriAgrawal <sridhar.agrawal@gmail.com>
2022-06-12 22:10:41 +0530

    Updates

---
+++ precache-manifest.45eeb0626045c1eb4d00df44d9f9ada5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "417e618353e1a1d0f227c67254e9cf48",
+    "url": "/InvoiceGenerate/index.html"
+  },
+  {
+    "revision": "71f652864ed89fc28b2a",
+    "url": "/InvoiceGenerate/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "67b47daa77256b5fb0fb",
+    "url": "/InvoiceGenerate/static/css/main.fb840a81.chunk.css"
+  },
+  {
+    "revision": "71f652864ed89fc28b2a",
+    "url": "/InvoiceGenerate/static/js/2.19545cdb.chunk.js"
+  },
+  {
+    "revision": "27eb1d1e8906a6c757af172b78c289d1",
+    "url": "/InvoiceGenerate/static/js/2.19545cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67b47daa77256b5fb0fb",
+    "url": "/InvoiceGenerate/static/js/main.9d76e574.chunk.js"
+  },
+  {
+    "revision": "8c5f3937d929915a6678",
+    "url": "/InvoiceGenerate/static/js/runtime-main.690d7a43.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/InvoiceGenerate/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/InvoiceGenerate/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/InvoiceGenerate/static/media/resize.d4da359f.svg"
+  }
+]);
