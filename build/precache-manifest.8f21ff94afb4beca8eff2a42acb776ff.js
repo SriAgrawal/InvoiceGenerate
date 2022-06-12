8845d5ab3b4a024cd97b79a4e57c5bc214b8b9b3
SriAgrawal <sridhar.agrawal@gmail.com>
2022-06-12 21:39:52 +0530

    invoice

---
+++ build/precache-manifest.8f21ff94afb4beca8eff2a42acb776ff.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1fe491836fdc5321f8bfbe4de5ea2a",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "d9ead7bd34ad57f5f835",
+    "url": "/react-invoice-generator/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "8a244149c75d296ca4a3",
+    "url": "/react-invoice-generator/static/css/main.1a8caee2.chunk.css"
+  },
+  {
+    "revision": "d9ead7bd34ad57f5f835",
+    "url": "/react-invoice-generator/static/js/2.15e75828.chunk.js"
+  },
+  {
+    "revision": "27eb1d1e8906a6c757af172b78c289d1",
+    "url": "/react-invoice-generator/static/js/2.15e75828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a244149c75d296ca4a3",
+    "url": "/react-invoice-generator/static/js/main.a9e7ade4.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/react-invoice-generator/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/react-invoice-generator/static/media/resize.d4da359f.svg"
+  }
+]);
